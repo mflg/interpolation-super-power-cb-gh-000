e93717d55f377d30b857ebede92142ff9dac7afc
Marcela Fontes Lima Guerra <marcela.guerra@gmail.com>
2018-07-07 20:21:29 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,14 +1,10 @@
 # Write your #display_rainbow method here
 def display_rainbow (colors)
   letters ["R: " ,", O: ", ", Y: ",", G: ", ", B: ", ", I: ", ", V: "]
-  print_letter_color(letter[0],color[0])
-  print_letter_color(letter[1],color[1])
-  print_letter_color(letter[2],color[2])
-  print_letter_color(letter[3],color[3])
-  print_letter_color(letter[4],color[4])
-  print_letter_color(letter[5],color[5])
-end
-
-def print_letter_color (letter, color)
-  print letter + color
+  print letters[0] + colors[0]
+  print letters[1] + colors[1]
+  print letters[2] + colors[2]
+  print letters[3] + colors[3]
+  print letters[4] + colors[4]
+  print letters[5] + colors[5]
 end
