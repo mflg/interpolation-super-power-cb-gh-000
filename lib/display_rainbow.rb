b5aafa9590683c173ec701a0b9f53652896d9f68
Marcela Fontes Lima Guerra <marcela.guerra@gmail.com>
2018-07-07 20:27:56 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -7,7 +7,8 @@ def display_rainbow (colors)
   print letters[3] + colors[3]
   print letters[4] + colors[4]
   print letters[5] + colors[5]
-  print letters[6] + colors[6]
+  puts letters[6] + colors[6]
+#R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet
 end
 
 display_rainbow (['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
